67a42ad653d7d2176fb94c288ea5206fbd25822d
Muhammad S. Ibrahim <hama.s.ibrahim9@gmail.com>
2024-05-06 18:59:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  // Return the fetch promise so .then() can be chained outside this function
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((books) => {
+      renderBooks(books);
+      return books; // Optionally return books if you need to use the response outside
+    });
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks();
 });
